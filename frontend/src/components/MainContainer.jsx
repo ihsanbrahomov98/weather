f9918b89b0fe5919f65e896e318a6dacf4ac725c
ihsanbrahomov98 <ihsanbrahomov@abv.bg>
2022-07-21 12:44:16 +0300

    css layoat changes

--- frontend/src/components/MainContainer.jsx
+++ frontend/src/components/MainContainer.jsx
@@ -1,12 +1,48 @@
 import React from "react";
-
+import SearchIcon from "@mui/icons-material/Search";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 import "./main.css";
 
 const MainContainer = () => {
   return (
     <>
       <div className="container">
-        <div className="containerWrapper"></div>
+        <div className="containerWrapper">
+          <div className="contentWrapper">
+            <div className="firstRow">
+              <span>Tokyo</span>
+              <span>Sydney</span>
+              <span>Tokyo</span>
+              <span>Sydney</span>
+              <span>Tokyo</span>
+            </div>
+            <div className="secondRow">
+              <input className="secondRowInput"></input>
+              <div className="secondRowIconWrapper">
+                <div className="secondRowSearchIcon">
+                  {" "}
+                  <SearchIcon />
+                </div>
+                <div className="secondLocationOnIcon">
+                  {" "}
+                  <LocationOnIcon />
+                </div>
+              </div>
+              <div className="secondRowDegreeWrapper">
+                <div>C</div>
+                <div>F</div>
+              </div>
+            </div>
+            <div className="thirdRow">1</div>
+            <div className="fourthRow">1</div>
+            <div className="fifthRow">1</div>
+            <div className="sixthRow">1</div>
+            <div className="seventhRow">1</div>
+            <div className="eightRow">1</div>
+            <div className="ninethRow">1</div>
+            <div className="tenthRow">1</div>
+          </div>
+        </div>
       </div>
     </>
   );
